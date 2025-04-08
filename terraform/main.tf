9d80178add1e35e2381ed7722ad25041e0abde83
Jon Howe <howe.jon@gmail.com>
2025-04-08 15:40:27 -0400

    latest code

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,23 @@ provider "aws" {
   region     = var.aws_region
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "nautobot_host" {
-  ami                         = "ami-0c02fb55956c7d316"
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   key_name                    = var.key_name
   associate_public_ip_address = true
